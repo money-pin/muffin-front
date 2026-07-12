8ef235c4682a0b5cd1d31019ab5ec6e37f294421
seokyun9 <westse05@gmail.com>
2026-07-13 00:04:09 +0900

    fix:Badge 공용 컴포넌트 bg 수정

--- src/components/common/Badge.tsx
+++ src/components/common/Badge.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 
 type BadgeVariant = "orange" | "gray" | "positive" | "negative";
 
-type BadgeSize = "sm" | "md";
+type BadgeSize = "sm" | "md" | "category";
 
 interface BadgeProps {
   children: ReactNode;
@@ -21,7 +21,7 @@ export default function Badge({
     "inline-flex items-center justify-center whitespace-nowrap rounded-[4px] text-center select-none";
 
   const variants: Record<BadgeVariant, string> = {
-    orange: "bg-primary-50 text-primary",
+    orange: "bg-secondary-100  text-primary",
     gray: "bg-neutral-50 text-neutral-700",
     positive: "bg-positive-50 text-positive",
     negative: "bg-negative-50 text-negative",
@@ -30,6 +30,7 @@ export default function Badge({
   const sizes: Record<BadgeSize, string> = {
     sm: "px-2 py-0.5 text-caption-12-md-tighter",
     md: "h-[22px] gap-1 pl-1.5 pr-1 py-1 text-caption-12-bd",
+    category: "h-6 px-2 py-1 text-caption-12-bd",
   };
 
   return (
