c04d66b5fe5a22c935d56de1e533885587c74521
seokyun9 <westse05@gmail.com>
2026-08-08 20:21:50 +0900

    style: 크라운 에셋 변경 및 컬러 수정

--- src/pages/invest/stats/components/RankBadge.tsx
+++ src/pages/invest/stats/components/RankBadge.tsx
@@ -3,15 +3,15 @@ interface RankBadgeProps {
 }
 
 const rankClassMap: Record<RankBadgeProps["rank"], string> = {
-  1: "bg-[#FFD400] text-[#331900]",
-  2: "bg-[#BABABA] text-neutral-0",
-  3: "bg-[#DE7813] text-neutral-0",
+  1: "bg-gold text-[#331900]",
+  2: "bg-silver text-neutral-0",
+  3: "bg-bronze text-neutral-0",
 };
 
 export default function RankBadge({ rank }: RankBadgeProps) {
   return (
     <span
-      className={`flex h-8 w-8 select-none items-center justify-center rounded-lg text-[16px] font-bold leading-none ${rankClassMap[rank]}`}
+      className={`flex h-8 w-8 items-center justify-center rounded-lg text-[16px] leading-none font-bold select-none ${rankClassMap[rank]}`}
     >
       {rank}
     </span>
